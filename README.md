4863cc1e7c25db54647593b31f864f58e52f34c8
Yabir Garcia <yabirg@protonmail.com>
2020-06-05 19:53:10 +0200

    Terminamos funcioens

--- README.md
+++ README.md
@@ -2,15 +2,34 @@
 
 Proyecto Final de la asignatura de aprendizaje automático
 
+## Estudio del Problema
+
+
+
 ## TO DO
 
 - Mirar si estan balanceadas las clases -> OK 75/25
 - Las clases con valores perdidos son : occupation (1843) , workclass (1836) y native-country (583). En test tenemos: occupation (967), workclass (964) ,native-country (275) y luego menos age todos tienen un valor perdido. Vamos a tomar la decisión de eliminar las instancias con valores perdidos.
 - fnlwgt: número de personas representadas en esa instancia. Poco interesante para el estudio.
 - Creamos los conjuntos de test y training 0.3 y 0.7 respectivamente. Mantenemos las proporciones 75/25
+- Había un problema con los espacios en las columnas y hemos solucionado esto leyendo los datos con una expresión regular y buscando valores
+  perdidos en dos casos
+- Eliminamos education-num por que es una representación numérica de el atributo education.
 
 
-# Modelos 
+## Preprocesado
+
+- [StandadScaler](https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html)
+- Hemos estudiado la matriz de correlaciones y la mayotia de los valores son ceros por lo que no podemos determinar ninguna correlación entre variables.
+- [VarianceThreshold](https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.VarianceThreshold.html)
+- Decisiones tomadas con respecto a los valores perdidos
+- Los dos atributos eliminados
+
+## Regularización
+
+- Usamos Lasso por que nuestros datos no están fuertemente correlados y con la decisión tomada con las variables dummy parece interesante usar Lasso
+
+## Modelos 
 
 Lineales:
 
@@ -25,4 +44,5 @@ No lineales:
 
 # Preguntas
 - Argumentar sobre la idoneidad de la función regularización usada 
-- Eliminación datos perdidos.
+- Outliyers
+- hemos visto que un modelo que gana en CV saca peor Etest que otro que pierde en CV
--- analysis.py
+++ analysis.py
@@ -4,6 +4,21 @@ import numpy as np
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+from sklearn import model_selection 
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression, LassoCV, Perceptron, SGDClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import VarianceThreshold, SelectFromModel
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.neural_network import MLPClassifier
+
 SEED = 1
 
 np.random.seed(SEED)
@@ -15,9 +30,9 @@ headers = ["age", "workclass", "fnlwgt", "education", "education-num",
 "marital-status", "occupation", "relationship", "race", "sex", "capital-gain",
 "captial-loss", "hours-per-week", "native-country", "income"]
 
-df = pd.read_csv(data_folder + "adult.data", index_col=False, delimiter=",", names=headers)
+df = pd.read_csv(data_folder + "adult.data", index_col=False, delimiter=",[ ]*", names=headers)
 
-df['income']=df['income'].map({' <=50K': 0, ' >50K': 1})
+df['income']=df['income'].map({'<=50K': 0, '>50K': 1})
 
 # mostrar info
 print(df.head())
@@ -25,6 +40,7 @@ print(len(df))
 
 # contar el numero que faltan
 df = df.replace(' ?', np.NaN)
+df = df.replace('?', np.NaN)
 print(df.isnull().sum(axis=0).sort_values(ascending = False).head(30))
 
 
@@ -32,12 +48,13 @@ print(df.isnull().sum(axis=0).sort_values(ascending = False).head(30))
 
 print(df.income.value_counts())
 
-df_test = pd.read_csv(data_folder + "adult.test", index_col=False, delimiter=",", names=headers)
+df_test = pd.read_csv(data_folder + "adult.test", index_col=False, delimiter=",[ ]*", names=headers)
 
 print(len(df_test))
+df_test = df_test.replace('?', np.NaN)
 df_test = df_test.replace(' ?', np.NaN)
 print(df_test.isnull().sum(axis=0).sort_values(ascending = False).head(30))
-df_test['income']=df_test['income'].map({' <=50K.': 0, ' >50K.': 1})
+df_test['income']=df_test['income'].map({'<=50K.': 0, '>50K.': 1})
 
 
 print(df.income.value_counts())
@@ -61,9 +78,11 @@ print(dataSet.income.value_counts()/45222)
 
 #eliminamos fnlwgt por que no aporta información
 dataSet = dataSet.drop(['fnlwgt'], axis=1)
+dataSet = dataSet.drop(['education-num'], axis=1)
+
 
 #Encoding categorical values
-dataSet["workclass"] = dataSet["workclass"].astype('category')
+""" dataSet["workclass"] = dataSet["workclass"].astype('category')
 dataSet["education"] = dataSet["education"].astype('category')
 dataSet["marital-status"] = dataSet["marital-status"].astype('category')
 dataSet["occupation"] = dataSet["occupation"].astype('category')
@@ -80,10 +99,27 @@ dataSet["occupation"] = dataSet["occupation"].cat.codes
 dataSet["relationship"] = dataSet["relationship"].cat.codes
 dataSet["race"] = dataSet["race"].cat.codes
 dataSet["sex"] = dataSet["sex"].cat.codes
-dataSet["native-country"] = dataSet["native-country"].cat.codes
+dataSet["native-country"] = dataSet["native-country"].cat.codes """
+
+cols_with_categories = ['workclass', 'education',"marital-status","occupation","relationship","race","sex","native-country"]
+
+for col in cols_with_categories:
+    print("Before: {} total of categories in {}".format(len(set(dataSet[col])), col))
+
+
+# convertir variables categoricas a dummies
+
+dataSet = pd.get_dummies(data=dataSet, columns=cols_with_categories)
+
+for asd in cols_with_categories:
+    cols = [col for col in dataSet if col.startswith(asd)]
+    print("After: {} total of categories in {}".format( len(cols), asd))
+
+print("==============")
 
 print(dataSet.head())
 
+print("==============")
 #Creamos las particiones train y test
 X, y = dataSet[dataSet.columns.difference(['income'])], dataSet['income']
 
@@ -93,11 +129,83 @@ train_x, test_x, train_y, test_y = train_test_split(X,y, test_size=0.3, stratify
 
 #vemos las proporciones de etiquetar post eliminar
 
-print("=====")
 print(test_y.value_counts()/len(test_y))
 print(train_y.value_counts()/len(train_y))
-print("=====")
 print(train_x.shape, test_x.shape)#vemos las proporciones de etiquetar post eliminar
 print(dataSet.income.value_counts()/45222)
 
-print(set(dataSet["capital-gain"]))
+#################
+#  Preprocesado #
+#################
+
+correlation_matrix = train_x.corr()
+#print (correlation_matrix)
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+sns.heatmap(correlation_matrix, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
+#plt.show()
+
+preproc=[
+            ("var", VarianceThreshold(0.01)),   
+            ("standardize", StandardScaler()),      
+            ("lasso", SelectFromModel(estimator=LassoCV(tol=0.001))),
+            ("poly",PolynomialFeatures(1)), 
+            ("var2", VarianceThreshold(0.01)),   
+        ]
+
+# estadisticas preprocesado
+
+pipe_pre=Pipeline(preproc)
+x_train_prep = pipe_pre.fit_transform(train_x, train_y)
+print("Descripción de los datos antes y después del preprocesado")
+print("Antes: {}".format(train_x.shape))
+print("Despues: {}".format(x_train_prep.shape))
+
+#creamos un pipeline de sklearn donde añadiremos uno de los modelos a estudiar
+pipe = Pipeline(steps=preproc+[('estimator', LogisticRegression())])
+
+# Añadimos los estimadores que vamos a utilizar y los parametros que vamos a estudiar:
+#       si clase de funcion lineal o cuadrática
+#       la potencia de la penalización l2 "C"    
+params_grid = [ {
+                'estimator':[LogisticRegression(max_iter=500)],
+                'estimator__solver':['lbfgs'],
+                'estimator__C': np.logspace(-4, 4, 3)
+                #'poly__degree': [1,2]
+                },
+                {
+                'estimator': [Perceptron(random_state = SEED)]
+                #'poly__degree': [1,2]
+                },
+                {
+                'estimator': [RandomForestClassifier(random_state = SEED)]
+                #'poly__degree': [1,2]
+                },
+                {
+                'estimator': [MLPClassifier(random_state = SEED)]
+                #'poly__degree': [1,2]
+                }
+                #{
+                #'estimator': [SVC()],
+                #'estimator__C': np.logspace(-4, 4, 3)
+                #'poly__degree': [1,2]
+                #}
+               # {'estimator':[Any_other_estimator_you_want],
+               #  'estimator__valid_param_of_your_estimator':[valid_values]
+
+              ]
+
+print("CON PREPROCESADO Y REGULARIZACION: \n")
+
+# entrenamos con crossvalidation y sacamos el mejor con sus parámetros.
+best_clf = GridSearchCV(pipe, params_grid, scoring = 'accuracy',cv = 5, n_jobs = -1, verbose=1)
+best_clf.fit(train_x, train_y)
+
+results=pd.DataFrame(best_clf.cv_results_)
+
+print("Mejor modelo:\n",best_clf.best_params_)
+print("Precisión en training:", 100.0 * best_clf.score(train_x, train_y))
+print("Precisión en test: ",100.0 * best_clf.score(test_x, test_y))
+
+print(results)
